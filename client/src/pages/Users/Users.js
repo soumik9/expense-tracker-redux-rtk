03cf019ed00555329a9b3624eae9b6ff5f244ea9
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-11-04 15:24:34 +0600

    react datatable

--- client/src/pages/Users/Users.js
+++ client/src/pages/Users/Users.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Breadcrumb from '../../components/Breadcrumb/Breadcrumb';
 import AddUser from './AddUser';
+import UserTable from './UserTable';
 
 const Users = () => {
 
@@ -12,7 +13,7 @@ const Users = () => {
             <Breadcrumb pageTitle={'Users'} pageLink={`/users`} secondLink={true} setIsOpen={setIsOpen} />
             
             <div className='bg-white border shadow-sm p-3 rounded-lg text-black mb-5'>
-                hello
+                <UserTable />
             </div>
 
             <AddUser isOpen={isOpen}setIsOpen={setIsOpen} />
